ca6e7049b76234b88d80280a010686ee6487b43f
Linky-js <113304680+Linky-js@users.noreply.github.com>
2022-10-11 15:30:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+function insert (num){
+    document.form.textview.value = document.form.textview.value + num;
+
+};
+function clean (){
+    document.form.textview.value = ""
+};
+function smooth () {
+    let i = document.form.textview.value;
+    if (i){
+        document.form.textview.value = eval(i);
+        if (eval(i) > 10000){
+            alert('Слишком дорого братан');
+        }
+    }
+};
 
