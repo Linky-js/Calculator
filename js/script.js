30268f8fb1bac954bbc4995d14c9a1208f840c02
Linky-js <113304680+Linky-js@users.noreply.github.com>
2022-10-12 17:15:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+function clockTimer(){
+  let date = new Date();
+  
+  let time = [date.getHours(),date.getMinutes(),date.getSeconds()]; 
+// |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
+  let dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  let days = date.getDay();
+  
+  if(time[0] < 10){time[0] = "0"+ time[0];}
+  if(time[1] < 10){time[1] = "0"+ time[1];}
+  if(time[2] < 10){time[2] = "0"+ time[2];}
+  
+  let currentTime = [time[0],time[1],time[2]].join(':');
+  let clock = document.getElementById("clock");
+  let day = document.getElementById("dayOfWeek");
+  
+  clock.innerHTML = currentTime;
+  day.innerHTML = dayOfWeek[days];
+  setTimeout("clockTimer()", 1000);
+};
+
 let display = document.querySelector('.display');
 
 function insert (num){
